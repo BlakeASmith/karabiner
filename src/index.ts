b2cda6bfedc9ff22ccd4da6452f2be96cbe97238
Blake Smith <blakeasm@amazon.com>
2025-11-09 21:58:42 -0800

    feat: setup some things

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,71 @@
 import {
+    ifApp,
+  ifVar,
   layer,
   map,
   NumberKeyValue,
   rule,
+  toSetVar,
+  withCondition,
   withMapper,
   writeToProfile,
 } from 'karabiner.ts'
 
-// ! Change '--dry-run' to your Karabiner-Elements Profile name.
-// (--dry-run print the config json into console)
-// + Create a new profile if needed.
-writeToProfile('--dry-run', [
-  // It is not required, but recommended to put symbol alias to layers,
-  // (If you type fast, use simlayer instead, see https://evan-liu.github.io/karabiner.ts/rules/simlayer)
-  // to make it easier to write '←' instead of 'left_arrow'.
-  // Supported alias: https://github.com/evan-liu/karabiner.ts/blob/main/src/utils/key-alias.ts
-  layer('/', 'symbol-mode').manipulators([
-    //     / + [ 1    2    3    4    5 ] =>
-    withMapper(['⌘', '⌥', '⌃', '⇧', '⇪'])((k, i) =>
-      map((i + 1) as NumberKeyValue).toPaste(k),
-    ),
-    withMapper(['←', '→', '↑', '↓', '␣', '⏎', '⇥', '⎋', '⌫', '⌦', '⇪'])((k) =>
-      map(k).toPaste(k),
-    ),
-  ]),
 
-  rule('Key mapping').manipulators([
-    // config key mappings
-    map(1).to(1)
-  ]),
-])
+const isTerminal = ifApp("^.*.iterm2.*$")
+const isNotTerminal = isTerminal.unless()
+
+
+const capsLock = rule("CapsLock for lots of things")
+	.manipulators([
+        // Use for tmux leader key if in a terminal application
+		withCondition(isTerminal)([
+            map("caps_lock").toIfAlone("a", "left_control")
+        ]),
+        withCondition(isNotTerminal)([
+            map("caps_lock").toIfAlone("caps_lock") // For now.....
+        ]),
+	])
+
+const shiftEsc = rule("Tap shift for Escape")
+    .manipulators([
+        map("left_shift").toIfAlone("escape")
+            .toIfHeldDown("left_shift")
+            .parameters({
+                // Needs to be very short not to interfere with typing
+                "basic.to_if_held_down_threshold_milliseconds": 60
+            })
+    ])
+
+
+const tab = layer("tab", "tab-mode")
+    .manipulators([
+        map("a").toVar("launcher-mode")
+            // .toAfterKeyUp(toSetVar("launcher-mode", 0))
+            .toNotificationMessage('launcher-mode-notification', 'launcher-mode')
+    ])
+
+    rule("tab for lots of things")
+    .manipulators([
+        map("tab").toIfAlone("tab")
+    ])
+
+
+const launcherMode = rule('Keybindings in launcher mode', ifVar('launcher-mode'))
+    .manipulators([
+        map('t')
+          .toAfterKeyUp(toSetVar("launcher-mode", 0))
+          .toRemoveNotificationMessage("launcher-mode-notification")
+          .toApp("ITerm")
+    ])
+
+
+writeToProfile('Default', [
+    capsLock,
+    shiftEsc,
+    tab,
+    launcherMode
+], {
+    // This seems to be the magic number for me for most things
+    "basic.to_if_held_down_threshold_milliseconds": 110
+})
